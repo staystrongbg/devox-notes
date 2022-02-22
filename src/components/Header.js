1f00a74e4444a67ca2c7d10db48376f87ddb4617
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-02-22 12:43:05 +0100

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({
   handleSelect,
 }) => {
   return (
-    <header className=' lg:flex lg:justify-around lg:items-start lg:flex-row  flex-col items-center justify-center gap-10 p-4 mt-20'>
+    <header className=' lg:flex lg:justify-around lg:items-start lg:flex-row  flex-col items-center justify-center p-4 mt-20'>
       <div className='flex flex-col items-center justify-center lg:justify-end lg:items-end gap-2 '>
         <h1 className='main-title text-6xl text-gray-900 '>devox notes</h1>
         <button
@@ -28,7 +28,7 @@ const Header = ({
         </button>
       </div>
       {user && (
-        <div className='flex  w-fit lg:gap-12 gap-4 bg-slate-600/50  rounded-lg p-4'>
+        <div className='flex  w-fit lg:gap-12 gap-4 bg-slate-600/50  rounded-lg p-4 mt-10 lg:mt-0'>
           <div>
             <Search handleSearch={handleSearch} />
             {user && newResult && (
