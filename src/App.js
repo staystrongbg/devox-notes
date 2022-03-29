72212ccf3007b89c73864b85e428d49eec07a063
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-03-29 20:13:27 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -224,7 +224,7 @@ function App() {
   return (
     <>
       <div
-        className={` w-full min-h-screen bg-gray-900  bg-cover bg-fixed overflow-x-hidden flex flex-col justify-between items-stretch `}
+        className={` px-2 w-full min-h-screen bg-gray-900  bg-cover bg-fixed overflow-x-hidden flex flex-col justify-between items-stretch `}
         // style={{ backgroundImage: `url(${bg})`, backgroundPosition: 'center' }}
       >
         {obavestenje && <Obavestenje obavestenje={obavestenje} />}
