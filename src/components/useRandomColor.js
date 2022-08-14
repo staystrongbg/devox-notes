1d2b3cc324ad42570ab788e57c2dc34613be7f10
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-08-14 18:41:55 +0200

    Delete src directory

--- src/components/useRandomColor.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-const useRandomColor = () => {
-  const [c, setC] = useState(Math.random().toString(16).substr(-6));
-
-  return { c };
-};
-
-export default useRandomColor;
